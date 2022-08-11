eedf77130e42d95aff66dfb40c2dfafe06da18eb
Oliver Tacke <o.tacke@posteo.de>
2022-08-11 12:22:52 +0200

    Add initial version

---
+++ src/entries/h5peditor-portfolio.js
@@ -0,0 +1,6 @@
+import "../styles/h5peditor-portfolio.css";
+import Portfolio from "../scripts/h5peditor-portfolio";
+
+// Load library
+H5PEditor = H5PEditor || {};
+H5PEditor.widgets.portfolio = Portfolio;
