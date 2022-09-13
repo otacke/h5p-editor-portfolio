724d6d656f319e491b362df86939a2c3a7fd559a
Oliver Tacke <o.tacke@posteo.de>
2022-09-13 15:47:13 +0200

    Use single quotes

--- src/entries/h5peditor-portfolio.js
+++ src/entries/h5peditor-portfolio.js
@@ -1,5 +1,5 @@
-import "../styles/h5peditor-portfolio.scss";
-import Portfolio from "../scripts/h5peditor-portfolio";
+import '../styles/h5peditor-portfolio.scss';
+import Portfolio from '../scripts/h5peditor-portfolio';
 
 // Load library
 H5PEditor = H5PEditor || {};
